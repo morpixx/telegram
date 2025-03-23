02010b93418e07931d9b99857b44df55a1c8552a
Rom Man <rom.man@s.fosbos-neumarkt.de>
2025-03-23 23:33:53 +0100

    new config

--- id_cheсker.py
+++ id_cheсker.py
@@ -1,18 +1,25 @@
 import asyncio
-from telethon import TelegramClient
+from pyrogram import Client
 
 # Замініть на ваші дані
 api_id = 27174235         # Наприклад, 123456
 api_hash = "93682bb767f2240d31252b764b6bfe9a"
 session = 'my_session'
 
-client = TelegramClient(session, api_id, api_hash)
+client = Client(session, api_id, api_hash)
 
 async def main():
-    # Отримуємо всі діалоги
-    dialogs = await client.get_dialogs()
-    for dialog in dialogs:
-        print(f"Назва: {dialog.name}, ID: {dialog.id}")
+    async for dialog in client.get_dialogs():
+        chat = dialog.chat
+        # Якщо це група або канал, використовується title,
+        # а якщо це приватний чат, використовується ім'я користувача.
+        if hasattr(chat, "title") and chat.title:
+            name = chat.title
+        elif hasattr(chat, "first_name") and chat.first_name:
+            name = chat.first_name + (" " + chat.last_name if hasattr(chat, "last_name") and chat.last_name else "")
+        else:
+            name = "Невідомо"
+        print(f"Назва: {name}, ID: {chat.id}")
 
 with client:
     client.loop.run_until_complete(main())
